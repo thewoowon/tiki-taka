1ff12eae59e7c150f22b88095f850e2aa4d8a8bc
thewoowon <thewoowon@naver.com>
2024-02-22 22:47:17 +0900

    feat: add analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,7 @@ import { useCallback, useEffect } from "react";
 import Analytics from "@/components/Analytics";
 import { usePathname } from "next/navigation";
 import * as gtag from "@/lib/gtag";
+import { GoogleAnalytics, GoogleTagManager } from "@next/third-parties/google";
 
 export default function RootLayout({
   children,
@@ -52,23 +53,6 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <Script id="google-analytics">
-          {`
-          (
-            function(w,d,s,l,i){
-              w[l]=w[l]||[];
-              w[l].push({
-                'gtm.start': new Date().getTime(),
-                event:'gtm.js'
-              });
-              var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
-              j.async=true;
-              j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
-              f.parentNode.insertBefore(j,f);
-            }
-          )(window,document,'script','dataLayer','${process.env.GTM_TRACKING_ID}');
-        `}
-        </Script>
         <title>{process.env.NEXT_PUBLIC_TITLE}</title>
         <meta property="og:title" content={process.env.NEXT_PUBLIC_TITLE} />
         <meta
@@ -197,14 +181,6 @@ export default function RootLayout({
         />
       </head>
       <body>
-        <noscript>
-          <iframe
-            src={`https://www.googletagmanager.com/ns.html?id=${gtag.GTM_TRACKING_ID}`}
-            height="0"
-            width="0"
-            style={{ display: "none", visibility: "hidden" }}
-          ></iframe>
-        </noscript>
         <Analytics />
         <QueryClientProvider client={queryClient}>
           <RecoilRoot>
@@ -215,6 +191,8 @@ export default function RootLayout({
           </RecoilRoot>
           <Toaster />
         </QueryClientProvider>
+        <GoogleAnalytics gaId={gtag.GA_TRACKING_ID || ""} />
+        <GoogleTagManager gtmId={gtag.GTM_TRACKING_ID || ""} />
       </body>
     </html>
   );

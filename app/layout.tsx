a562f96c58a9832a373e42722af2bfaac7c20785
thewoowon <thewoowon@naver.com>
2024-02-22 20:08:02 +0900

    fix: script tag move to head

--- app/layout.tsx
+++ app/layout.tsx
@@ -52,6 +52,23 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <Script id="google-analytics">
+          {`
+          (
+            function(w,d,s,l,i){
+              w[l]=w[l]||[];
+              w[l].push({
+                'gtm.start': new Date().getTime(),
+                event:'gtm.js'
+              });
+              var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
+              j.async=true;
+              j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+              f.parentNode.insertBefore(j,f);
+            }
+          )(window,document,'script','dataLayer','${gtag.GTM_TRACKING_ID}');
+        `}
+        </Script>
         <title>{process.env.NEXT_PUBLIC_TITLE}</title>
         <meta property="og:title" content={process.env.NEXT_PUBLIC_TITLE} />
         <meta
@@ -182,7 +199,7 @@ export default function RootLayout({
       <body>
         <noscript>
           <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-WMPN6NGH"
+            src={`https://www.googletagmanager.com/ns.html?id=${gtag.GTM_TRACKING_ID}`}
             height="0"
             width="0"
             style={{ display: "none", visibility: "hidden" }}

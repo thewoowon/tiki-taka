acbfb44253ce05fa41b7f5250253b7600f6e651d
thewoowon <thewoowon@naver.com>
2024-05-20 08:43:05 +0900

    feat: 스터디 문구 변경

--- app/study/page.tsx
+++ app/study/page.tsx
@@ -3,7 +3,7 @@
 import { Loading } from "@/components/View/Loading";
 
 const StudyPage = () => {
-  return <Loading title="준비 중이에요 🥳" description="조금만 기다려주세요!" />;
+  return <Loading title="오픈 준비 중이에요 🥳" description="조금만 기다려주세요!" />;
 };
 
 export default StudyPage;

2692d7e52e93fc6a1ad27cd959af8800ab25087e
thewoowon <thewoowon@naver.com>
2024-06-27 18:07:57 +0900

    feat: video background ending

--- components/Element/BackgroundVideo/BackgroundVideo.tsx
+++ components/Element/BackgroundVideo/BackgroundVideo.tsx
@@ -17,6 +17,7 @@ const BackgroundVideo = () => {
         <source src="/videos/background.mp4" type="video/mp4" />
         비디오를 지원하지 않는 브라우저입니다.
       </Video>
+      <Shade />
     </Container>
   );
 };
@@ -41,4 +42,29 @@ const Video = styled.video`
   height: 100%;
   object-fit: cover;
   transform: translate(-50%, -50%);
+`;
+
+const Shade = styled.div`
+  background: radial-gradient(
+      137.47% 43.57% at 50% 50%,
+      rgba(0, 0, 0, 0) 0%,
+      rgba(0, 0, 0, 0.2) 100%
+    ),
+    linear-gradient(0deg, rgba(0, 0, 0, 0.1) 0%, rgba(0, 0, 0, 0.1) 100%),
+    linear-gradient(
+      187deg,
+      rgba(0, 0, 0, 0) 56.14%,
+      rgba(0, 0, 0, 0.1) 72.48%,
+      #000 98.3%
+    );
+  background-blend-mode: normal, normal, normal;
+  background-position: 1.123px -839.09px, center, center;
+  background-repeat: no-repeat, no-repeat, no-repeat;
+  background-size: 100.158% 53.767%, cover, cover;
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: 1;
 `;

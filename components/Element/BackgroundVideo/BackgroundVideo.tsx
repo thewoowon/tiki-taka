1839dda0fe002f26c3dd61832c04137e60724afa
thewoowon <thewoowon@naver.com>
2024-06-30 19:26:05 +0900

    feat: m background video

--- components/Element/BackgroundVideo/BackgroundVideo.tsx
+++ components/Element/BackgroundVideo/BackgroundVideo.tsx
@@ -1,22 +1,62 @@
 // components/BackgroundVideo.js
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import LoadingSpinner from "../LoadingSpinner";
 import styled from "@emotion/styled";
 
 const BackgroundVideo = () => {
   const [isLoading, setIsLoading] = useState(true);
+  const videoRef = useRef<HTMLVideoElement>(null);
+  const mVideoRef = useRef<HTMLVideoElement>(null);
 
   const handleCanPlay = () => {
     setIsLoading(false);
   };
 
+  useEffect(() => {
+    const video = videoRef.current;
+    if (video) {
+      video.play().catch((error) => {
+        console.error("비디오 재생 중 오류가 발생했습니다.", error);
+      });
+    }
+  }, []);
+
+  useEffect(() => {
+    const mVideo = mVideoRef.current;
+    if (mVideo) {
+      mVideo.play().catch((error) => {
+        console.error("비디오 재생 중 오류가 발생했습니다.", error);
+      });
+    }
+  }, []);
+
   return (
     <Container>
       {isLoading && <LoadingSpinner />}
-      <Video autoPlay muted loop onCanPlay={handleCanPlay} preload="auto">
+      <Video
+        ref={videoRef}
+        autoPlay
+        muted
+        loop
+        onCanPlay={handleCanPlay}
+        preload="auto"
+      >
         <source src="/videos/background.mp4" type="video/mp4" />
         비디오를 지원하지 않는 브라우저입니다.
       </Video>
+      <MVideo
+        ref={mVideoRef}
+        autoPlay
+        muted
+        loop
+        onCanPlay={handleCanPlay}
+        preload="auto"
+      >
+        <source src="/videos/m-background.mp4" type="video/mp4" />
+        비디오를 지원하지 않는 브라우저입니다.
+      </MVideo>
+
+      <Shade />
     </Container>
   );
 };
@@ -41,4 +81,47 @@ const Video = styled.video`
   height: 100%;
   object-fit: cover;
   transform: translate(-50%, -50%);
+
+  @media (max-width: 768px) {
+    display: none;
+  }
+`;
+
+const MVideo = styled.video`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  transform: translate(-50%, -50%);
+
+  @media (min-width: 768px) {
+    display: none;
+  }
+`;
+
+const Shade = styled.div`
+  background: radial-gradient(
+      137.47% 43.57% at 50% 50%,
+      rgba(0, 0, 0, 0) 0%,
+      rgba(0, 0, 0, 0.2) 100%
+    ),
+    linear-gradient(0deg, rgba(0, 0, 0, 0.1) 0%, rgba(0, 0, 0, 0.1) 100%),
+    linear-gradient(
+      187deg,
+      rgba(0, 0, 0, 0) 56.14%,
+      rgba(0, 0, 0, 0.1) 72.48%,
+      #000 98.3%
+    );
+  background-blend-mode: normal, normal, normal;
+  background-position: 1.123px -839.09px, center, center;
+  background-repeat: no-repeat, no-repeat, no-repeat;
+  background-size: 100.158% 53.767%, cover, cover;
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: 1;
 `;
